7aac351720d662550ec024f68469a4207c53174d
AG <agibbison@pm.me>
2022-12-11 14:50:56 +0000

    Formatting

--- src/thoughts/pages.tsx
+++ src/thoughts/pages.tsx
@@ -1,68 +1,68 @@
-import { Component, h } from 'nano-jsx';
-import { Homepage } from './pages/Homepage.tsx';
-import { Ideas } from './pages/Ideas.tsx';
-import { AGI } from './pages/AGI.tsx';
-import { FailureEngineer } from './pages/FailureEngineer.tsx';
-import { Navigation } from './components/Navigation.tsx';
-import type { TNavigation, TPage } from '../@types/thoughts.d.ts';
-import { AdditionPrimeFactors } from './pages/AdditionPrimeFactors.tsx';
-import { CareerProgression } from './pages/CareerProgression.tsx';
-import { Nurbs } from './pages/Nurbs.tsx';
-import { ProblemsMentalHealthAwareness } from './pages/ProblemsMentalHealthAwareness.tsx';
-import { ProgrammingAxioms } from './pages/ProgrammingAxioms.tsx';
+import { Component, h } from "nano-jsx";
+import { Homepage } from "./pages/Homepage.tsx";
+import { Ideas } from "./pages/Ideas.tsx";
+import { AGI } from "./pages/AGI.tsx";
+import { FailureEngineer } from "./pages/FailureEngineer.tsx";
+import { Navigation } from "./components/Navigation.tsx";
+import type { TNavigation, TPage } from "../@types/thoughts.d.ts";
+import { AdditionPrimeFactors } from "./pages/AdditionPrimeFactors.tsx";
+import { CareerProgression } from "./pages/CareerProgression.tsx";
+import { Nurbs } from "./pages/Nurbs.tsx";
+import { ProblemsMentalHealthAwareness } from "./pages/ProblemsMentalHealthAwareness.tsx";
+import { ProgrammingAxioms } from "./pages/ProgrammingAxioms.tsx";
 
 export function generateThoughtPages() {
   const rawPages: (TNavigation & { Page: (_: TPage) => Component })[] = [
     {
-      path: 'index',
-      title: 'Homepage',
+      path: "index",
+      title: "Homepage",
       Page: Homepage,
     },
-    { path: 'ideas', title: 'Ideas', Page: Ideas },
+    { path: "ideas", title: "Ideas", Page: Ideas },
     {
-      path: 'hypothesis-fundamental-agi',
+      path: "hypothesis-fundamental-agi",
       title:
-        '~ Hypothesis for the fundamentals of an Artificial General Intelligence',
+        "~ Hypothesis for the fundamentals of an Artificial General Intelligence",
       Page: AGI,
     },
     {
-      path: 'failure-engineer',
-      title: '~ Failure as a Software Engineer',
+      path: "failure-engineer",
+      title: "~ Failure as a Software Engineer",
       Page: FailureEngineer,
     },
     {
-      path: 'addition-prime-factors',
-      title: '~ Addition and Prime Factors',
+      path: "addition-prime-factors",
+      title: "~ Addition and Prime Factors",
       Page: AdditionPrimeFactors,
     },
     {
-      path: 'career-progression',
-      title: '~ Career Progression',
+      path: "career-progression",
+      title: "~ Career Progression",
       Page: CareerProgression,
     },
     {
-      path: 'nurbs',
-      title: '~ Nurbs',
+      path: "nurbs",
+      title: "~ Nurbs",
       Page: Nurbs,
     },
     {
-      path: 'mental-health',
-      title: '~ Problems with Mental Health Awareness',
+      path: "mental-health",
+      title: "~ Problems with Mental Health Awareness",
       Page: ProblemsMentalHealthAwareness,
     },
     {
-      path: 'programming-axioms',
-      title: '~ Axioms for Good Code',
+      path: "programming-axioms",
+      title: "~ Axioms for Good Code",
       Page: ProgrammingAxioms,
     },
   ];
 
-  const navigation = <Navigation pages={rawPages}/>;
+  const navigation = <Navigation pages={ rawPages } />;
 
   return rawPages.map((
     { path, title, Page },
   ) => ({
     path,
-    page: <Page title={title} Navigation={navigation}/>,
+    page: <Page title={ title } Navigation={ navigation } />,
   }));
 }
